f533338a6a25631ab655a4062062955d86102e41
bilalsert <sertbilal@gmail.com>
2019-11-13 19:02:42 -0600

    Objects and Methods

---
+++ BilalFirstJava/src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("BilalSert123");
+		System.out.println("HelloWorld");
+		System.out.println("BISMILLAHIRRAHMANIRRAHIM");
+		System.out.println("SelamIsmailHocam NasssinIyimisn");
+		System.out.println("bismillah");
+		System.out.println("bismillahherhayrinbasidirbizdahionunlabaslariz");
+	}
+
+}
